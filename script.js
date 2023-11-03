0f2653ebbe31078e174ab8e498b2ce66389aaeb7
xonin-hush <xonindev@gmail.com>
2023-11-03 20:16:35 +0000

    Add from/dialog

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ function Book(title,author,pagesNum,readStatus) {
   this.pagesNum=pagesNum
   this.readStatus=readStatus
 }
-const atomicHabits= new Book("atomic habits","james clear","544","read")
-const somethingNew= new Book("new book","james clear","544","read")
-myLibrary.push(atomicHabits)
-myLibrary.push(somethingNew)
-console.log(myLibrary)
-console.log(atomicHabits)
+//random books (fixed)
+// const atomicHabits= new Book("atomic habits","james clear","544","read")
+// const somethingNew= new Book("new book","james clear","544","read")
+// myLibrary.push(atomicHabits)
+// myLibrary.push(somethingNew)
+// console.log(myLibrary)
+// console.log(atomicHabits)
+
 function addBookToLibrary() {
   // do stuff here
 }
+// some dialog code
+const dialog=document.querySelector("dialog")
+const showButton=document.querySelector("#show-button")
+const hideButton=document.querySelector("#hide-button")
+showButton.addEventListener("click",()=>{
+dialog.showModal()
+});
+hideButton.addEventListener("click",()=>{
+  dialog.close()
+})
+
