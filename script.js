e78ca172a1953453fe33b3c5fb6cfdc994fe3252
xonin-hush <xonindev@gmail.com>
2023-11-10 19:14:01 +0000

    Push constructor into myLibrary using function

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 const myLibrary = [];
-
-function Book(title, author, pagesNum, readStatus) {
-  this.title = title
-  this.author = author
-  this.pagesNum = pagesNum
-  this.readStatus = readStatus
-}
-//random books (fixed)
-// const atomicHabits= new Book("atomic habits","james clear","544","read")
-// const somethingNew= new Book("new book","james clear","544","read")
-// myLibrary.push(atomicHabits)
-// myLibrary.push(somethingNew)
-// console.log(myLibrary)
-// console.log(atomicHabits)
-
-//form input 
-
-
-function addBookToLibrary() {
-  // do stuff here
-}
-// some dialog code
 const dialog = document.querySelector("dialog")
 const showButton = document.querySelector("#show-button")
 const cancel = dialog.querySelector("#cancel")
 const submit = dialog.querySelector("#submit")
+addBookToLibrary()
 showButton.addEventListener("click", () => {
   dialog.showModal()
 });
 
-cancel.addEventListener("click", () => {
-  dialog.close()
-})
+function addBookToLibrary() {
+  submit.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    var title = document.querySelector("#book-name")
+    var temp = title.value
+    var author = document.querySelector("#author-name")
+    var pages = document.querySelector("#pages-number")
+    var readStatus = document.querySelector("#read-status")
+    console.log(title.value, author.value, pages.value, readStatus.value)
+    temp = new Book(title.value, author.value, pages.value, readStatus.value)
+    console.log('temp', temp)
+    myLibrary.push(temp)
+    console.log(title)
+    console.log('myLibrary', myLibrary)
+  });
+  return title.value;
+}
 
 submit.addEventListener("change", (e) => {
   confirmBtn.value = selectEl.value;
@@ -43,23 +36,14 @@ dialog.addEventListener("close", (e) => {
       ? "No return value."
       : `ReturnValue: ${dialog.returnValue}.`
 })
+//dialog.close(selectEl.value); // Have to send the select box value here.
+function Book(title, author, pagesNum, readStatus) {
+  this.title = title
+  this.author = author
+  this.pagesNum = pagesNum
+  this.readStatus = readStatus
+}
 
-submit.addEventListener("click", (event) => {
-  event.preventDefault(); // We don't want to submit this fake form
-  var name1 = document.querySelector("#book-name")
-  var author = document.querySelector("#author-name")
-  var pages = document.querySelector("#pages-number")
-  var status1 = document.querySelector("#read-status")
-  console.log(name1.value, author.value, pages.value, status1.value)
-  //dialog.close(selectEl.value); // Have to send the select box value here.
-  function Book(title, author, pagesNum, readStatus) {
-    this.title = title
-    this.author = author
-    this.pagesNum = pagesNum
-    this.readStatus = readStatus
-  }
-  var name1 = new this.Book(name1.value, author.value, pages.value, status1.value)
-  myLibrary.push(name1)
-  console.log(name1)
-  console.log(myLibrary)
-});
+cancel.addEventListener("click", () => {
+  dialog.close()
+})
