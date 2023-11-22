08a9bc0e2c7acde28ff716521ceeac18c845b0f2
xonin-hush <xonindev@gmail.com>
2023-11-22 19:50:50 +0000

    Add text to displayed info

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const myLibrary = [];
 // Book = {
 //   author: "1414",
-//   pagesNum: "141",
+//   pages: "141",
 //   readStatus: "on",
 //   title: "123"
 // }, Book = {
 //   author: "something",
-//   pagesNum: "141",
+//   pages: "141",
 //   readStatus: "on",
 //   title: "something?"
 // }, Book = {
 //   author: "something",
-//   pagesNum: "141",
+//   pages: "141",
 //   readStatus: "on",
 //   title: "something?"
 // }
@@ -35,50 +35,54 @@ function addBookToLibrary() {
     var author = document.querySelector("#author-name")
     var pages = document.querySelector("#pages-number")
     var readStatus = document.querySelector("#read-status")
-    console.log(title.value, author.value, pages.value, readStatus.value)
     temp = new Book(title.value, author.value, pages.value, readStatus.value)
-    console.log('temp', temp)
     myLibrary.push(temp)
-    console.log(title)
-    console.log('myLibrary', myLibrary)
     dialog.close(dialog.value); // Have to send the select box value here.
     showBooks()
   });
 }
 
 
-function Book(title, author, pagesNum, readStatus) {
+function Book(title, author, pages, readStatus) {
   this.title = title
   this.author = author
-  this.pagesNum = pagesNum
+  this.pages = pages
   this.readStatus = readStatus
 }
 
 function showBooks() {
-  while (cards.firstChild) {
+
+  while (cards.firstChild) { //prevent cards to duplicate
     cards.removeChild(cards.lastChild);
   }
-  myLibrary.forEach(Book => {
+
+  myLibrary.forEach(Book => { //creates each card in array
     let card = document.createElement('div')
     cards.appendChild(card)
     card.classList.add('card')
+
     for (let element in Book) {
-      let test = document.createElement('div')
-      test.textContent = Book[element]
-      card.appendChild(test)
-      console.log(Book[element])
+      let text = document.createElement('div')
+     if(element=="title"){
+      text.textContent = "Title: "+Book[element]
+      card.appendChild(text)
+     }
+     if(element=="author"){
+      text.textContent = "Author: "+Book[element]
+      card.appendChild(text)
+     }
+     if(element=="pages"){
+      text.textContent = "Pages: "+Book[element]
+      card.appendChild(text)
+     }
+     if(element=="readStatus"){
+      text.textContent = "Status: "+Book[element]
+      card.appendChild(text)
+     }
+
     }
   });
 }
-// submit.addEventListener("change", (e) => {
-//   confirmBtn.value = selectEl.value;
-// });
-// dialog.addEventListener("close", (e) => {
-//   outputBox.value =
-//     dialog.returnValue === "default"
-//       ? "No return value."
-//       : `ReturnValue: ${dialog.returnValue}.`
-// })
 cancel.addEventListener("click", () => {
 
   dialog.close()
